1498ecdbfb3d1580dc0658355e3b3bba8120caad
codeMUI <uiqbal@gmail.com>
2020-07-22 22:11:41 +0200

    UI-1 websocket example code from https://howtoprogram.xyz/2016/12/24/websocket-client-example-okhttp/

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -1,7 +1,23 @@
 package com.test;
 
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.WebSocket;
+
 public class Test {
-    public static void main(String[] args){
-        System.out.println("Very poorly written code");
+    public static void main(String... args) {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder().url("ws://echo.websocket.org").build();
+        EchoWebSocketListener listener = new EchoWebSocketListener();
+
+        WebSocket ws = client.newWebSocket(request, listener);
+
+
+        // Trigger shutdown of the dispatcher's executor so this process can
+        // exit cleanly.
+        client.dispatcher().executorService().shutdown();
     }
 }
